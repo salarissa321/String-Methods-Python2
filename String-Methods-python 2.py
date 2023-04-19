d91be69ae1fb10e6bf4d62ce858d330b7a1ab896
Salar Issa <120121093+salarissa321@users.noreply.github.com>
2023-04-19 12:54:08 +0200

    Update String-Methods-python 2.py

--- String-Methods-python 2.py
+++ String-Methods-python 2.py
@@ -6,96 +6,96 @@
 
 # index(SubString, Start, End)
 a = "I Love Python"
-print(a.index("P", 0, 10))
+print(a.index("P", 0, 10)) # 7
 
-print(a.index("P", 0, 5))
+print(a.index("P", 0, 5)) # ValueError: substring not found
 
 
 # find(SubString, Start, End)
 
 b = "I Love Python"
-print(b.find("P", 0, 10))
+print(b.find("P", 0, 10)) # 7
 
-print(b.find("P", 0, 5))
+print(b.find("P", 0, 5)) # -1
 
 
 # rjust(Width, Fillchar) 
 
 s = "salar"
-print(s.rjust(10))
-print(s.rjust(10 , "#"))
+print(s.rjust(10)) #      salar
+print(s.rjust(10 , "#")) # #####salar
 
 # ljust(Width, Fillchar)
 
 s = "salar"
-print(s.ljust(10))
-print(s.ljust(10 , "#"))
+print(s.ljust(10)) # salar     
+print(s.ljust(10 , "#")) # salar#####
 
 
 # Splitlines()
 e = ''' First Line
 Second Line
 Third Line'''
-print(e.splitlines())
+print(e.splitlines()) # [' First Line', 'Second Line', 'Third Line']
 
 # istitle()
 one = "I Love Python And 3G"
 two = "I Love Python And 3g"
-print(one.istitle())
-print(two.istitle())
+print(one.istitle()) # True
+print(two.istitle()) # False
 
 # isspace()
 three = " "
 four = ""
-print(three.isspace())
-print(four.isspace())
+print(three.isspace()) # True
+print(four.isspace()) # False
 
 # islower()
 five = "i love python"
 six = "I Love Python"
 
-print(five.islower())
-print(six.islower())
+print(five.islower()) # True
+print(six.islower()) # False
 
 # isidentifier()
 
 seven = "salarissa"
 eight = "Salar-issa"
 nine = "salarissa1995"
-print(seven.isidentifier())
-print(eight.isidentifier())
-print(nine.isidentifier())
+print(seven.isidentifier()) # True
+print(eight.isidentifier()) # False 
+print(nine.isidentifier()) # True
 
 
 # isalpha()
 x = "AaaaaaBbbbbbbb"
 y = "AaaaaaBbbbbbbb4739539"
-print(x.isalpha())
-print(y.isalpha())
+print(x.isalpha()) # True
+print(y.isalpha()) # False
 
 # isalnum()
 
-t = "AaaaaaBbbbbbbb"
-g = "AaaaaaBbbbbbbb4739539"
-print(t.isalnum())
-print(g.isalnum())
+t = "AaaaaaBbbbbbbb" 
+g = "AaaaaaBbbbbbbb4739539" 
+print(t.isalnum()) # True
+print(g.isalnum()) # True 
 
 
 # replace(Old Value, New Value , Count)
 
 a = "Hello One Two Three One One"
-print(a.replace("One", "1", ))
-print(a.replace("One", "1", 1 ))
-print(a.replace("One", "1", 2 ))
+print(a.replace("One", "1", )) # Hello 1 Two Three 1 1
+print(a.replace("One", "1", 1 )) # Hello 1 Two Three One One
+print(a.replace("One", "1", 2 )) # Hello 1 Two Three 1 One
 
 
 # Join(Iterable)
 
 mylist = ["Salar" , "Issa" , "Issa"]
-print("-".join(mylist))
-print(" ".join(mylist))
-print(" , ".join(mylist))
-print(type(" , ".join(mylist)))
+print("-".join(mylist)) # Salar-Issa-Issa
+print(" ".join(mylist)) # Salar Issa Issa
+print(" , ".join(mylist)) #  Salar , Issa , Issa
+print(type(" , ".join(mylist))) # <class 'str'>
 
 
 
